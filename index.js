e2d335cf8736b34624c4438326708ecef243c75e
Giuseppe <giuseppe.gatto@caffeina.com>
2022-03-22 09:57:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let Parser = require('rss-parser');
+let parser = new Parser();
+const { Telegraf } = require('telegraf')
+
+async function analyzeFeed(){
+    let feed= []
+    let boolEnd = false
+    const baseUrl = 'https://www.immobiliare.it/vendita-case/lucca/?criterio=dataModifica&ordine=desc&prezzoMassimo=180000&localiMinimo=3&idMZona[]=62&idMZona[]=10409&idMZona[]=10411&idMZona[]=64&idQuartiere[]=147&idQuartiere[]=12409&idQuartiere[]=205&idQuartiere[]=12405&mode=rss'
+    let count = 1
+    while (!boolEnd){
+        try {
+            let url = baseUrl
+            if (count > 1 ) url = `${baseUrl}&pag=${count}`
+            const result =  await parser.parseURL(url);
+            feed.push(...result.items)
+         
+         } catch (error) {
+            boolEnd = true
+         }
+         count++
+    }
+
+    // questa roba penso(credo) si possa ottimizzare con una promise all rimane il dubbio che non so come prendere tutte le pagine 
+   return feed
+
+}
+const bot = new Telegraf('5281784241:AAEqU3qH-yDsgWy5uEDGZHEbKcymjeiq-5s')//process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.launch()
